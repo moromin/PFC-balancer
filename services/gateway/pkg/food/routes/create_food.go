168767315df0aad09fbc7a69e8fbbedf3d1b201f
moromin <34294957+moromin@users.noreply.github.com>
2022-04-05 18:51:06 +0900

    feat: add validation for CreateFood request body

--- services/gateway/pkg/food/routes/create_food.go
+++ services/gateway/pkg/food/routes/create_food.go
@@ -5,21 +5,29 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/moromin/PFC-balancer/services/food/proto"
 )
 
 type CreateFoodRequestBody struct {
-	Name         string  `json:"name"`
-	Protein      float64 `json:"protein"`
-	Fat          float64 `json:"fat"`
-	Carbohydrate float64 `json:"carbohydrate"`
-	Category     int64   `json:"category"`
+	Name         string  `json:"name" validate:"required"`
+	Protein      float64 `json:"protein" validate:"required,gte=0"`
+	Fat          float64 `json:"fat" validate:"required,gte=0"`
+	Carbohydrate float64 `json:"carbohydrate" validate:"required,gte=0"`
+	Category     int64   `json:"category" validate:"required"`
 }
 
 func CreateFood(ctx *gin.Context, c proto.FoodServiceClient) {
 	body := CreateFoodRequestBody{}
 
 	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	validate := validator.New()
+	err := validate.Struct(body)
+	if err != nil {
 		ctx.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
