d75bf8d893525dc4cef1ece9eeabfbe43a3c8abb
moromin <34294957+moromin@users.noreply.github.com>
2022-04-08 14:36:53 +0900

    fix: service name

--- services/recipe/proto/recipe_grpc.pb.go
+++ services/recipe/proto/recipe_grpc.pb.go
@@ -14,120 +14,120 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// FoodServiceClient is the client API for FoodService service.
+// RecipeServiceClient is the client API for RecipeService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type FoodServiceClient interface {
+type RecipeServiceClient interface {
 	CreateRecipe(ctx context.Context, in *CreateRecipeRequest, opts ...grpc.CallOption) (*CreateRecipeResponse, error)
 	ReadRecipe(ctx context.Context, in *ReadRecipeRequest, opts ...grpc.CallOption) (*ReadRecipeResponse, error)
 }
 
-type foodServiceClient struct {
+type recipeServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewFoodServiceClient(cc grpc.ClientConnInterface) FoodServiceClient {
-	return &foodServiceClient{cc}
+func NewRecipeServiceClient(cc grpc.ClientConnInterface) RecipeServiceClient {
+	return &recipeServiceClient{cc}
 }
 
-func (c *foodServiceClient) CreateRecipe(ctx context.Context, in *CreateRecipeRequest, opts ...grpc.CallOption) (*CreateRecipeResponse, error) {
+func (c *recipeServiceClient) CreateRecipe(ctx context.Context, in *CreateRecipeRequest, opts ...grpc.CallOption) (*CreateRecipeResponse, error) {
 	out := new(CreateRecipeResponse)
-	err := c.cc.Invoke(ctx, "/food.FoodService/CreateRecipe", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/food.RecipeService/CreateRecipe", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *foodServiceClient) ReadRecipe(ctx context.Context, in *ReadRecipeRequest, opts ...grpc.CallOption) (*ReadRecipeResponse, error) {
+func (c *recipeServiceClient) ReadRecipe(ctx context.Context, in *ReadRecipeRequest, opts ...grpc.CallOption) (*ReadRecipeResponse, error) {
 	out := new(ReadRecipeResponse)
-	err := c.cc.Invoke(ctx, "/food.FoodService/ReadRecipe", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/food.RecipeService/ReadRecipe", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// FoodServiceServer is the server API for FoodService service.
-// All implementations should embed UnimplementedFoodServiceServer
+// RecipeServiceServer is the server API for RecipeService service.
+// All implementations should embed UnimplementedRecipeServiceServer
 // for forward compatibility
-type FoodServiceServer interface {
+type RecipeServiceServer interface {
 	CreateRecipe(context.Context, *CreateRecipeRequest) (*CreateRecipeResponse, error)
 	ReadRecipe(context.Context, *ReadRecipeRequest) (*ReadRecipeResponse, error)
 }
 
-// UnimplementedFoodServiceServer should be embedded to have forward compatible implementations.
-type UnimplementedFoodServiceServer struct {
+// UnimplementedRecipeServiceServer should be embedded to have forward compatible implementations.
+type UnimplementedRecipeServiceServer struct {
 }
 
-func (UnimplementedFoodServiceServer) CreateRecipe(context.Context, *CreateRecipeRequest) (*CreateRecipeResponse, error) {
+func (UnimplementedRecipeServiceServer) CreateRecipe(context.Context, *CreateRecipeRequest) (*CreateRecipeResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateRecipe not implemented")
 }
-func (UnimplementedFoodServiceServer) ReadRecipe(context.Context, *ReadRecipeRequest) (*ReadRecipeResponse, error) {
+func (UnimplementedRecipeServiceServer) ReadRecipe(context.Context, *ReadRecipeRequest) (*ReadRecipeResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ReadRecipe not implemented")
 }
 
-// UnsafeFoodServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to FoodServiceServer will
+// UnsafeRecipeServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to RecipeServiceServer will
 // result in compilation errors.
-type UnsafeFoodServiceServer interface {
-	mustEmbedUnimplementedFoodServiceServer()
+type UnsafeRecipeServiceServer interface {
+	mustEmbedUnimplementedRecipeServiceServer()
 }
 
-func RegisterFoodServiceServer(s grpc.ServiceRegistrar, srv FoodServiceServer) {
-	s.RegisterService(&FoodService_ServiceDesc, srv)
+func RegisterRecipeServiceServer(s grpc.ServiceRegistrar, srv RecipeServiceServer) {
+	s.RegisterService(&RecipeService_ServiceDesc, srv)
 }
 
-func _FoodService_CreateRecipe_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RecipeService_CreateRecipe_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateRecipeRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FoodServiceServer).CreateRecipe(ctx, in)
+		return srv.(RecipeServiceServer).CreateRecipe(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/food.FoodService/CreateRecipe",
+		FullMethod: "/food.RecipeService/CreateRecipe",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FoodServiceServer).CreateRecipe(ctx, req.(*CreateRecipeRequest))
+		return srv.(RecipeServiceServer).CreateRecipe(ctx, req.(*CreateRecipeRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _FoodService_ReadRecipe_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RecipeService_ReadRecipe_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ReadRecipeRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(FoodServiceServer).ReadRecipe(ctx, in)
+		return srv.(RecipeServiceServer).ReadRecipe(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/food.FoodService/ReadRecipe",
+		FullMethod: "/food.RecipeService/ReadRecipe",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(FoodServiceServer).ReadRecipe(ctx, req.(*ReadRecipeRequest))
+		return srv.(RecipeServiceServer).ReadRecipe(ctx, req.(*ReadRecipeRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// FoodService_ServiceDesc is the grpc.ServiceDesc for FoodService service.
+// RecipeService_ServiceDesc is the grpc.ServiceDesc for RecipeService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var FoodService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "food.FoodService",
-	HandlerType: (*FoodServiceServer)(nil),
+var RecipeService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "food.RecipeService",
+	HandlerType: (*RecipeServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "CreateRecipe",
-			Handler:    _FoodService_CreateRecipe_Handler,
+			Handler:    _RecipeService_CreateRecipe_Handler,
 		},
 		{
 			MethodName: "ReadRecipe",
-			Handler:    _FoodService_ReadRecipe_Handler,
+			Handler:    _RecipeService_ReadRecipe_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
