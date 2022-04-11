873ffe4743b74c2bbd613081846012c069cb607e
moromin <34294957+moromin@users.noreply.github.com>
2022-04-11 13:10:40 +0900

    feat: basic user service

---
+++ services/user/grpc/grpc.go
@@ -0,0 +1,33 @@
+package grpc
+
+import (
+	"context"
+
+	pkggrpc "github.com/moromin/PFC-balancer/pkg/grpc"
+	db "github.com/moromin/PFC-balancer/platform/db/proto"
+	"github.com/moromin/PFC-balancer/services/user/proto"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+func RunServer(ctx context.Context, port int, l *zap.Logger) error {
+	opts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	}
+
+	conn, err := grpc.DialContext(ctx, "localhost:5000", opts...)
+	if err != nil {
+		return err
+	}
+
+	dbClient := db.NewDBServiceClient(conn)
+
+	svc := &server{
+		dbClient: dbClient,
+	}
+
+	return pkggrpc.NewServer(port, func(s *grpc.Server) {
+		proto.RegisterUserServiceServer(s, svc)
+	}).Start(ctx)
+}
