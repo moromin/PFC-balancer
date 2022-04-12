d0b7a146eb4c5b183717261e230dff12c8435f91
moromin <34294957+moromin@users.noreply.github.com>
2022-04-12 19:03:04 +0900

    feat: menu service that only handle food

---
+++ services/menu/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/moromin/PFC-balancer/pkg/logger"
+	"github.com/moromin/PFC-balancer/services/menu/grpc"
+)
+
+func main() {
+	os.Exit(run(context.Background()))
+}
+
+func run(ctx context.Context) int {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, os.Kill)
+	defer stop()
+
+	l, err := logger.New()
+	if err != nil {
+		_, ferr := fmt.Fprintf(os.Stderr, "failed to create logger: %s", err)
+		if ferr != nil {
+			log.Fatalf("failed to write log: %s, original error: %s", err, ferr)
+		}
+		return 1
+	}
+
+	errCh := make(chan error, 1)
+	go func() {
+		errCh <- grpc.RunServer(ctx, 50053, l)
+	}()
+
+	select {
+	case err := <-errCh:
+		log.Println(err)
+		return 1
+	case <-ctx.Done():
+		log.Println("shutting down...")
+		return 0
+	}
+}
